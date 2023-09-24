8cbb45db4fee11553668a7396c2734b4896c952c
ALT-JS <1797069243@qq.com>
2023-09-24 13:11:43 +0800

    loadtest

--- loadtest.py
+++ loadtest.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-dataset = np.load("W:\ho_datasets\CHAIRS\AHOI_Data\DATA_FOLDER\object_root_location.npy")
+dataset = np.load("W:\ho_datasets\CHAIRS\AHOI_Data\DATA_FOLDER\object_root_rotation.npy")
 
-np.savetxt("obj_root_location.txt", dataset)
+np.savetxt("obj_root_rotation.txt", dataset)
